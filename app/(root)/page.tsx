a1757c059741d0e70e56a078cb7c7158122da699
ermiastsegu <ermiastsegu@yahoo.com>
2024-02-15 10:20:18 +0300

    shadcn configuration tested.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { icons } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div>
+      <Button size="default" variant="destructive">
+        Hello
+      </Button>
+    </div>
+  );
+}
