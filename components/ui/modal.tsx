814738d2237045c15fe9d5b9196f49ebae633ccc
ermiastsegu <ermiastsegu@yahoo.com>
2024-02-20 15:33:31 +0300

    reusable modal component added.

---
+++ components/ui/modal.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  //DialogTrigger,
+} from "@/components/ui/dialog";
+
+interface modalProps {
+  isOpen: boolean;
+  title: string;
+  description: string;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+export const Modal: React.FC<modalProps> = ({
+  isOpen,
+  title,
+  description,
+  onClose,
+  children,
+}) => {
+  const onChange = (open: boolean) => {
+    if (!isOpen) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      {/* <DialogTrigger asChild>
+        <Button variant="outline">Edit Profile</Button>
+      </DialogTrigger> */}
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
