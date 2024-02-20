0c345960cc9e375ea6d8ac18c3c500486a4cbf80
ermiastsegu <ermiastsegu@yahoo.com>
2024-02-20 16:47:35 +0300

    store modal created.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,18 @@
 "use client";
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { stat } from "fs";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    // <Modal title="test" isOpen description="test" onClose={() => {}}>
-    //   children
-    // </Modal>
-    <div>
-      <h1>Hello</h1>
-      <Modal
-        isOpen={true}
-        title="Edit your Profile"
-        description="Make changes to your profile here. Click save when you're done."
-        onClose={() => {}}
-      >
-        children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal((stat) => stat.onOpen);
+  const isOpen = useStoreModal((stat) => stat.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  return <div>Root Page</div>;
 }
